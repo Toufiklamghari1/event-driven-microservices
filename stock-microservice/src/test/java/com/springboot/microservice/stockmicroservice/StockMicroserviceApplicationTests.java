8fb89d222f33b7e933835da8cf2e29d5dbb88254
Toufik lamghari <64041688+Toufiklamghari1@users.noreply.github.com>
2025-02-24 14:32:13 +0000

    create Stock microservice

---
+++ stock-microservice/src/test/java/com/springboot/microservice/stockmicroservice/StockMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.microservice.stockmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
